e476a633e3f799b2c0d8482b535051b26fc479d1
adamsyy <adamoommenjacob@gmail.com>
2022-09-26 18:40:38 +0530

    remove nav

--- watermark_remove.js
+++ watermark_remove.js
@@ -9,7 +9,3 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }else{}
     },0.1);
   }) 
-  window.onscroll = () => {
-    const nav = document.querySelector('#navbar');
-    if(this.scrollY <= 10) nav.className = 'navigation w-nav'; else nav.className = 'navbar-container-scroll';
-  };
